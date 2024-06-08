9a4cb8ab90a17349ab06dc3ed0a11186642318c8
franco22bellido <franco.bellido22@gmail.com>
2024-06-08 19:20:12 -0300

    upload photo in memory buffer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import * as formidable from 'formidable';
 import sharp from 'sharp';
-import fs from 'fs/promises'
 import { config } from 'dotenv'
 import { v2 as cloudinary } from 'cloudinary'
+import multer from 'multer'
+
 config()
 
 cloudinary.config(
@@ -15,24 +15,18 @@ cloudinary.config(
 )
 
 const app = express()
+const upload = multer()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/api/upload', async (req, res, next) => {
-    const form = new formidable.IncomingForm()
+app.post('/api/upload',upload.single('photo'), async (req, res) => {
 
     try {
-        const [fields, files] = await form.parse(req);
-        const file = files.myfile[0]
-        console.log(file.filepath)
-        sharp.cache(false)
-
-        const imagenCompressed = await sharp(file.filepath)
+        const imagenCompressed = await sharp(req.file.buffer)
             .jpeg({ quality: 30 })
             .resize({ width: 1080, height: 1350, fit: 'cover', position: 'center' })
             .toBuffer()
-        
         const response = await new Promise((resolve, reject)=> {
             cloudinary.uploader.upload_stream({}, (err, result)=> {
                 if(err) {
@@ -41,8 +35,8 @@ app.post('/api/upload', async (req, res, next) => {
                 return resolve(result)
             }).end(imagenCompressed)
         })
-        
-        res.json({ ok: true, message: "photo uploaded", url: response.secure_url })
+
+        res.json({ ok: true, message: "photo uploaded", url: response.secure_url})
     } catch (error) {
         console.log(error)
         return res.json(error)
