6c155e7836623f1570fe5ebf858621bb57c975a7
franco22bellido <franco.bellido22@gmail.com>
2024-06-09 16:24:57 -0300

    change dependencie sharp

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv'
-const sharp = require('@img/sharp-win32-x64/sharp.node')
+const sharp = require('@img/sharp-linux-arm64/sharp.node')
 import { v2 as cloudinary } from 'cloudinary'
 import multer from 'multer'
 import cors from 'cors'
