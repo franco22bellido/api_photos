353bf546395c2665a6e24315ea4bc7a366cbd896
franco22bellido <franco.bellido22@gmail.com>
2024-06-14 01:08:18 -0300

    commit changes

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ const app = express()
 const upload = multer()
 
 
-app.use(cors({origin: [keys.FRONT_END_HOST]}))
-app.use(express.json({limit: '15mb'}))
-app.use(express.urlencoded({ extended: true, limit: '15mb'}))
+app.use(cors({ origin: [keys.FRONT_END_HOST] }))
+app.use(express.json({ limit: '15mb' }))
+app.use(express.urlencoded({ extended: true, limit: '15mb' }))
 
 const allowedExtensions = ['jpg', 'jpeg', 'png', 'bmp']
-app.get('/', (req, res)=> {
-    res.json({message: "hello world"})
+app.get('/', (req, res) => {
+    res.json({ message: "hello world" })
 })
 
-app.delete('/api/photo/public_id', async (req, res)=>{
+app.delete('/api/photo/public_id', async (req, res) => {
     try {
         const { public_id } = req.params
         const response = await cloudinary.uploader.destroy(public_id)
@@ -41,14 +41,20 @@ app.delete('/api/photo/public_id', async (req, res)=>{
 app.post('/api/photo', upload.single('photo'), async (req, res) => {
     const maxSize = 1024 * 1024 * 15;
     try {
-    if(req.file.size>maxSize) return res.status(413).json({message: "the max size is 15 megabytes"})
-        
-        const fileExtension = req.file.mimetype.split('/')[1]
-    if (!allowedExtensions.includes(fileExtension)) return res.status(415).json({ ok: false, message: 'unsupported format' })
+        if (req.file.size > maxSize) return res.status(413).json({ message: "the max size is 15 megabytes" })
 
+        //divir la resolucion de la imagen
+        const fileExtension = req.file.mimetype.split('/')[1]
+        if (!allowedExtensions.includes(fileExtension)) return res.status(415).json({ ok: false, message: `unsupported format: ${fileExtension}` })
+
+        const { width, height } = await sharp(req.file.buffer).metadata()
+        let newHeight, newWidth;
+
+        newHeight = 1350;
+        newWidth = Math.round((width / height) * newHeight);
         const imagenCompressed = await sharp(req.file.buffer)
             .jpeg({ quality: 30 })
-            .resize({ width: 1080, height: 1350, fit: 'cover', position: 'center' })
+            .resize({ width: newWidth, height: newHeight, fit: 'cover', position: 'center' })
             .toBuffer()
 
         const response = await new Promise((resolve, reject) => {
